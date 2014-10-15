81151a9991fa8af5b53d4223d568ec33f905cd0f
beggath81 <beggath81@gmail.com>
2014-10-15 10:15:14 +0000

    Test - NegitiveNumbers - ok

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -6,35 +6,40 @@ import org.junit.Test;
 public class CalculatorTest {
 
 	@Test
-	public void testEmptyString() {
+	public void testEmptyString() throws Exeption{
 		assertEquals(0, Calculator.add(""));
 	}
 
 	@Test
-	public void testOneNumber(){
+	public void testOneNumber()throws Exeption{
 		assertEquals(1, Calculator.add("1"));
 	}
 	@Test
-	public void testTwoNumbers(){
+	public void testTwoNumbers()throws Exeption{
 		assertEquals(3, Calculator.add("1,2"));
 	}
 
 	@Test
-	public void testMultipleNumbers(){
+	public void testMultipleNumbers()throws Exeption{
 		assertEquals(6, Calculator.add("1,2,3"));
 	}
 
 	@Test
-	public void test3NumbersWitNewLine(){
+	public void test3NumbersWitNewLine()throws Exeption{
 		assertEquals(6, Calculator.add("1\n2,3"));
 	}
 @Test
-	public void testTwoNumbersNewline(){
+	public void testTwoNumbersNewline()throws Exeption{
 		assertEquals(3, Calculator.add("1\n2"));
 	}
 
 	@Test
-	public void testDiffrDelimiter(){
+	public void testDiffrDelimiter()throws Exeption{
 		assertEquals(3, Calculator.add("//;\n1;2"));
+	}
+
+	@Test
+	public void testNegitiveNumber()throws Exeption{
+		assertEquals(3, Calculator.add("-1"));
 	}
 }
